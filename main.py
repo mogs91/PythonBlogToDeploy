b987c489ef076f2a3aed5f50a5abbfa7c133a0c2
Mogs91 <markossayan@gmail.com>
2023-01-03 16:50:10 +0100

    Implementing POSTGresQL

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from functools import wraps
 
 import werkzeug
@@ -24,7 +25,7 @@ from sqlalchemy.ext.declarative import declarative_base
 import datetime as dt
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -40,7 +41,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',  'sqlite:///blog.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
